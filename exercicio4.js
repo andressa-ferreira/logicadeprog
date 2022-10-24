e924b2d41f6d6345ff8d85865fbaacc1a1e96068
Andressa <andressasanfe@gmail.com>
2022-10-24 17:55:14 -0300

    Segundo commit

---
+++ exercicio4.js
@@ -0,0 +1,12 @@
+/* Crie um programa que verifique se um determinado número é impar ou par!
+Caso seja ímpar retorne "O número X é ímpar"
+Caso seja par retorne "O número X é ímpar"
+
+Dica do sucesso: todo número par é divisível por 2.*/
+
+let descubra = 56;
+if descubra = %2 == 0 {
+    console.log ("Esse número é par")} 
+    else
+    {console.log ("Esse número é ímpar")
+    }
