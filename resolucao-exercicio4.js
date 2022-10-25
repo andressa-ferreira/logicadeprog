8df4b5cd610358bfbf278abee4fbd3e9da87e407
Andressa <andressasanfe@gmail.com>
2022-10-25 18:35:30 -0300

    quarto commit

---
+++ resolucao-exercicio4.js
@@ -0,0 +1,23 @@
+/*Crie um programa que leia a idade do nadador e o classifique em uma das categorias:
+"Infantil A": 5 a 7 anos
+"Infantil B": 8 a 11 anos
+"Juvenil A": 12 a 13 anos
+"Juvenil B": 14 a 17 anos
+"Adulto": maiores de 18 anos
+
+Caso a pessoa não tenha idade para nenhuma das categorias
+o programa deve retornar "Você ainda não pode competir!" */
+
+let idade = 6;
+if (idade >= 5) ||( idade <= 7 );{
+console.log ("Infantil A")
+} 
+ else if ( idade >= 8 ) || ( idade <= 11 ); {
+    console.log ("Infantil B")
+} else  if ( idade>=12) || ( idade <= 13); {
+    console.log ("Juvenil A")
+}  else if (idade >=14) || ( idade <=17); {
+    console.log ("Juvenil B")
+} else if (idade = 18) {
+    console.log ("Adulto")
+}
